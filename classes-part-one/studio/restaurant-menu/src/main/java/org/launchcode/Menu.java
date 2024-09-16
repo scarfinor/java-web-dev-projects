5c23126632fb30a244a460538fc2079f0b5cc502
scarfinor <scarfinor@gmail.com>
2024-09-16 18:48:42 -0500

    git commit added to Student.java: Menu.java class

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,8 +1,31 @@
 package org.launchcode;
 
+import java.util.Date;
 import java.util.ArrayList;
+import org.launchcode.MenuItem;
 
 public class Menu {
-    private ArrayList<String> MenuItems;
+    private ArrayList<MenuItem> items;
+    private Date lastUpdated;
 
+    public Menu() {
+        items = new ArrayList<>();
+        lastUpdated = new Date();
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+        lastUpdated = new Date();
+    }
+
+    public void removeItem(MenuItem item) {
+        items.remove(item);
+        lastUpdated = new Date();
+    }
+
+    public void displayMenu() {
+        for (MenuItem item : items) {
+            System.out.println(item);
+        }
+    }
 }
