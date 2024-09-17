dd10f636288327548b6af2f665baef8595f75b2a
scarfinor <scarfinor@gmail.com>
2024-09-16 20:35:58 -0500

    git commit added to Student.java: printMenuItem() printMenu()

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -2,6 +2,8 @@ package org.launchcode;
 
 import java.util.Date;
 import java.util.ArrayList;
+import java.util.Objects;
+
 import org.launchcode.MenuItem;
 
 public class Menu {
@@ -27,5 +29,38 @@ public class Menu {
         for (MenuItem item : items) {
             System.out.println(item);
         }
+    }
+
+    public void printMenu() {
+        System.out.println("Menu (last updated: " + lastUpdated + ")");
+        for (MenuItem item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public void printMenuItem(String value) {
+        System.out.println("Menu Item: " + items);
+
+        for (MenuItem item : items) {
+            if(value.contains((CharSequence) item)) {
+            System.out.println(value);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Menu menu)) return false;
+        return Objects.equals(items, menu.items) && Objects.equals(lastUpdated, menu.lastUpdated);
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "items=" + items +
+                ", lastUpdated=" + lastUpdated +
+                '}';
     }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,16 +1,23 @@
 package org.launchcode;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class MenuItem {
     private double price;
     private String description;
     private String category;
     private boolean isNew;
+    private Date dateAdded;
+
 
     public MenuItem(double price, String description, String category, boolean isNew) {
         this.price = price;
         this.description = description;
         this.category = category;
         this.isNew = isNew;
+        this.dateAdded = new Date();
+
     }
 
     public double getPrice() {
@@ -27,6 +34,7 @@ public class MenuItem {
 
     public void setDescription(String description) {
         this.description = description;
+        this.dateAdded = new Date();
     }
 
     public String getCategory() {
@@ -42,10 +50,30 @@ public class MenuItem {
     }
 
     public void setNew(boolean aNew) {
+
         isNew = aNew;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Double.compare(getPrice(), menuItem.getPrice()) == 0 && isNew() == menuItem.isNew() && Objects.equals(getDescription(), menuItem.getDescription()) && Objects.equals(getCategory(), menuItem.getCategory()) && Objects.equals(dateAdded, menuItem.dateAdded);
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "price=" + price +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", isNew=" + isNew +
+                ", dateAdded=" + dateAdded +
+                '}';
+    }
 }
+
 
 
 
