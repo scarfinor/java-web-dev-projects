9c167f43e710fc0f1c7ae1bf3af45cad0279ed33
scarfinor <scarfinor@gmail.com>
2024-10-01 21:42:30 -0500

    git commit finished walkthrough class 9

--- exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
+++ exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
@@ -1,5 +1,9 @@
 package org.launchcode;
 
-public class TemperatureException {
+public class TemperatureException extends Exception{
     // Write code here!
+    double absoluteZeroFahrenheit = -459.67;
+    public TemperatureException(String message) {
+        super(message);
+    }
 }
