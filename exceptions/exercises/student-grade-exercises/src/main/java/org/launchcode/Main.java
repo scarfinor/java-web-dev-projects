53ef94d5c2bd324730a4caa25c31d81e6ee86699
scarfinor <scarfinor@gmail.com>
2024-10-01 22:09:51 -0500

    git commit finished exercises: exceptions

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,17 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        try {
+            System.out.println(Divide(10,2));
+            System.out.println(Divide(10,0));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,16 +20,35 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> entry : studentFiles.entrySet()) {
+            try {
+                int points = CheckFileExtension(entry.getValue());
+                System.out.println(entry.getKey() + ": " + points + "points");
+            } catch (Exception e) {
+                System.out.println(entry.getKey() + ": " + e.getMessage());
+            }
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+        try {
+            return  x/y;
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
+    public static int CheckFileExtension(String fileName) throws Exception
     {
         // Write code here!
+        if (fileName == null || fileName.isEmpty()) {
+            throw new IllegalArgumentException("File name cannot be null or empty");
+        }
+        if (fileName.endsWith(".java")) {
+            return 1;
+        }
         return 0;
     }
 }
